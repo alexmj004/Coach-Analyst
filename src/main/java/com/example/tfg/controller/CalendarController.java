08f67fa27cdbc0c48b196a7cf247bab25892f24e
David RM <rm8david@gmail.com>
2025-04-01 20:45:41 +0200

    calendario, hora de inicio y de fin

--- src/main/java/com/example/tfg/controller/CalendarController.java
+++ src/main/java/com/example/tfg/controller/CalendarController.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 public class CalendarController {
@@ -26,9 +28,20 @@ public class CalendarController {
         Button resetButton = (Button) calendarScene.lookup("#reset");
         ChoiceBox<String> categoriaBox = (ChoiceBox<String>) calendarScene.lookup("#categoria");
         TextField locationField = (TextField) calendarScene.lookup("#location");
+        ComboBox<String> startTImeBox = (ComboBox<String>) calendarScene.lookup("#startTime");
+        ComboBox<String> endTimeBox = (ComboBox<String>) calendarScene.lookup("#endTime");
 
         //agregar categorias al CHoiceBox
         categoriaBox.getItems().addAll("Match", "Training", "Meeting", "Other");
+        //inicializar combobox de horas
+        for (int hour = 0; hour < 24; hour++){
+            for (int minute = 0; minute <60; minute += 30){
+                String time = String.format("%02d:%02d", hour, minute);
+                startTImeBox.getItems().add(time);
+                endTimeBox.getItems().add(time);
+            }
+        }
+
 
         // mostrar la fecha actual en el DatePicker
         LocalDate today = LocalDate.now();
@@ -42,9 +55,11 @@ public class CalendarController {
             LocalDate selectedDate = datePicker.getValue();
             String category = categoriaBox.getValue();
             String location = locationField.getText();
+            String starTime = startTImeBox.getValue();
+            String endTime = endTimeBox.getValue();
 
-            if (!appointmentText.isEmpty() && selectedDate != null) {
-                addAppointment(appointmentText, selectedDate, appointmentsArea, category, location);
+            if (!appointmentText.isEmpty() && selectedDate != null && starTime != null && endTime != null) {
+                addAppointment(appointmentText, selectedDate, starTime,endTime, appointmentsArea, category, location);
                 appointmentField.clear();
             } else {
                 showAlert("Datos incompletos", "Por favor seleccione una fecha y escriba una descripción");
@@ -55,6 +70,8 @@ public class CalendarController {
             appointmentField.clear();
             appointmentsArea.clear();
             datePicker.setValue(null);
+            startTImeBox.setValue(null);
+            endTimeBox.setValue(null);
         });
 
         datePicker.setOnAction(e -> {
@@ -65,15 +82,15 @@ public class CalendarController {
         });
     }
 // TODO: implementa la hora de inicio y fin
-    private void addAppointment(String description, LocalDate date, TextArea appointmentsArea, String category, String location) {
+    private void addAppointment(String description, LocalDate date,String startTime,String endTime, TextArea appointmentsArea, String category, String location) {
         try {
             Calendar newEvent = new Calendar();
             newEvent.setTitle("Evento");
             newEvent.setDescription(description);
-            Timestamp startTimestamp = Timestamp.valueOf(date.atStartOfDay());
-            newEvent.setStart(startTimestamp);
-            Timestamp endTimestamp = Timestamp.valueOf(date.atStartOfDay().plusDays(1));
-            newEvent.setEnd(endTimestamp);
+            LocalDateTime startDateTime = LocalDateTime.of(date, LocalTime.parse(startTime));
+            LocalDateTime endDateTime = LocalDateTime.of(date, LocalTime.parse(endTime));
+            newEvent.setStart(Timestamp.valueOf(startDateTime));
+            newEvent.setEnd(Timestamp.valueOf(endDateTime));
             newEvent.setCategory(category);
             newEvent.setLocation(location);
 
@@ -85,7 +102,7 @@ public class CalendarController {
             showAlert("Error", "No se pudo añadir el evento: " + e.getMessage());
         }
     }
-//TODO implementar lo mismo que en la insercion, categoria, localizacion, hora de inicio y hora de fin
+//TODO implementar lo mismo que en la insercion hora de inicio y hora de fin
     private void loadAppointmentsByDate(LocalDate date, TextArea appointmentsArea) {
         try {
             List<Calendar> events = calendarService.findByDay(date);
@@ -98,7 +115,8 @@ public class CalendarController {
                     sb.append("Título: ").append(event.getTitle()).append("\n");
                     sb.append("Descripción: ").append(event.getDescription()).append("\n");
                     sb.append("Fecha: ").append(event.getStart().toLocalDateTime().toLocalDate()).append("\n");
-
+                    sb.append("Hora inicio: ").append(event.getStart().toLocalDateTime().toLocalTime()).append("\n");
+                    sb.append("Hora fin: ").append(event.getEnd().toLocalDateTime().toLocalTime()).append("\n");
                     sb.append("Categoría: ").append(event.getCategory()).append("\n");
                     sb.append("Localización: ").append(event.getLocation()).append("\n");
                     sb.append("-----------------------------------\n");
