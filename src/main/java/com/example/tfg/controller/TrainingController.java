cd352125859bd6bbd3a7e5bc4b4f12620932f3da
David RM <rm8david@gmail.com>
2025-04-03 18:14:52 +0200

    cambios en el campo de texto en TRAINING

--- src/main/java/com/example/tfg/controller/TrainingController.java
+++ src/main/java/com/example/tfg/controller/TrainingController.java
@@ -14,11 +14,13 @@ public class TrainingController {
     @Autowired
     private TrainingService trainingService;
 
+    //TODO implementar un metodo que permita visualizar todos los entrenamientos y preguntar para que sirve el boton "Modificar"
+
     public void setupTrainingComponents(Scene trainingScene) {
         DatePicker datePicker = (DatePicker) trainingScene.lookup("#calendar_training");
         TextField locationField = (TextField) trainingScene.lookup("#location");
         TextField objectiveField = (TextField) trainingScene.lookup("#objective_training");
-        TextField notesField = (TextField) trainingScene.lookup("#notes");
+        //TextField notesField = (TextField) trainingScene.lookup("#notes");
         TextArea trainingDescription = (TextArea) trainingScene.lookup("#description_training");
         Button addButton = (Button) trainingScene.lookup("#accept_btn");
         Button modifyButton = (Button) trainingScene.lookup("#modify_btn");
@@ -31,10 +33,10 @@ public class TrainingController {
             String location = locationField.getText();
             LocalDate eventDate = datePicker.getValue();
             String objective = objectiveField.getText();
-            String notesText = notesField.getText();
+            String description = trainingDescription.getText();
 
-            if (!location.isEmpty() && eventDate != null && !objective.isEmpty()) {
-                addTraining(location, eventDate, objective, notesText);
+            if (!location.isEmpty() && eventDate != null && !objective.isEmpty() && !description.isEmpty()) {
+                addTraining(location, eventDate, objective, description);
                 showAlert("Entrenamiento agregado", "El entrenamiento se ha añadido correctamente");
             } else {
                 showAlert("Datos incompletos", "Por favor seleccione una fecha y escriba una descripción");
@@ -42,14 +44,14 @@ public class TrainingController {
         });
     }
 
-    private void addTraining(String location, LocalDate eventDate, String objective, String notesText) {
+    private void addTraining(String location, LocalDate eventDate, String objective, String description) {
         try {
             Training newTraining = new Training();
             newTraining.setLocation(location);
             java.sql.Date sqlDate = java.sql.Date.valueOf(eventDate);
             newTraining.setDate(sqlDate);
             newTraining.setObjective(objective);
-            newTraining.setNotes(notesText);
+            newTraining.setNotes(description);
 
             trainingService.saveTraining(newTraining);
 
